a35ad062d45bb348780183076b512744e213c8af
Wayaer <wayaer@foxmail.com>
2020-10-21 13:33:34 +0800

    no message

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_umeng/flutter_umeng.dart';
+import 'package:flutter_umeng/fl_umeng.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  UMeng.init(androidAppKey: '5f895b5a94846f78a9749090', iosAppKey: '5f89595094846f78a9748eca', channel: "channel");
-  UMeng.setLogEnabled(true);
+
+  ///注册友盟
+  initUM(androidAppKey: '5f895b5a94846f78a9749090', iosAppKey: '5f89595094846f78a9748eca', channel: 'channel');
+
+  ///是否开启log
+  setLogEnabledUM(true);
   runApp(MaterialApp(
     title: 'UMeng example',
     theme: ThemeData(
@@ -18,15 +22,6 @@ void main() {
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('UMeng example'),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [Center(child: Text('UMeng 初始化完成'))],
-      ),
-    );
+    return Scaffold(appBar: AppBar(title: const Text('UMeng example')), body: const Center(child: Text('UMeng 初始化完成')));
   }
 }
--- lib/fl_umeng.dart
+++ lib/fl_umeng.dart
@@ -4,72 +4,70 @@ import 'package:flutter/services.dart';
 
 const MethodChannel _channel = MethodChannel('UMeng');
 
-class UMeng {
-  ///初始化
-  static Future<void> init({String androidAppKey, String iosAppKey, String channel}) async {
-    if (_supportPlatform()) return;
-    _channel.invokeMethod<dynamic>(
-        'init', <String, String>{'androidAppKey': androidAppKey, 'iosAppKey': iosAppKey, 'channel': channel});
-  }
+///初始化
+Future<void> initUM({String androidAppKey, String iosAppKey, String channel}) async {
+  if (_supportPlatform()) return;
+  _channel.invokeMethod<dynamic>(
+      'init', <String, String>{'androidAppKey': androidAppKey, 'iosAppKey': iosAppKey, 'channel': channel});
+}
 
-  ///是否开启日志
-  static Future<void> setLogEnabled(bool logEnabled) async {
-    if (_supportPlatform()) return;
-    _channel.invokeMethod<dynamic>('setLogEnabled', <String, bool>{'logEnabled': logEnabled});
-  }
+///是否开启日志
+Future<void> setLogEnabledUM(bool logEnabled) async {
+  if (_supportPlatform()) return;
+  _channel.invokeMethod<dynamic>('setLogEnabled', <String, bool>{'logEnabled': logEnabled});
+}
 
-  /// 发送自定义事件（目前属性值支持字符、整数、浮点、长整数，暂不支持NULL、布尔、MAP、数组）
-  static Future<void> onEvent(String event, Map<String, dynamic> properties) async {
-    if (_supportPlatform()) return;
-    _channel.invokeMethod<dynamic>('onEvent', <String, dynamic>{'event': event, 'properties': properties});
-  }
+/// 发送自定义事件（目前属性值支持字符、整数、浮点、长整数，暂不支持NULL、布尔、MAP、数组）
+Future<void> onEventUM(String event, Map<String, dynamic> properties) async {
+  if (_supportPlatform()) return;
+  _channel.invokeMethod<dynamic>('onEvent', <String, dynamic>{'event': event, 'properties': properties});
+}
 
-  /// 设置用户账号
-  static Future<void> onProfileSignIn(String userID) async {
-    if (_supportPlatform()) return;
-    _channel.invokeMethod<dynamic>('onProfileSignIn', <String, String>{'userID': userID});
-  }
+/// 设置用户账号
+Future<void> onProfileSignInUM(String userID) async {
+  if (_supportPlatform()) return;
+  _channel.invokeMethod<dynamic>('onProfileSignIn', <String, String>{'userID': userID});
+}
 
-  /// 取消用户账号
-  static Future<void> onProfileSignOff() async {
-    if (_supportPlatform()) return;
-    _channel.invokeMethod<dynamic>('onProfileSignOff');
-  }
+/// 取消用户账号
+Future<void> onProfileSignOffUM() async {
+  if (_supportPlatform()) return;
+  _channel.invokeMethod<dynamic>('onProfileSignOff');
+}
 
-  /// 如果需要使用页面统计，则先打开该设置
-  static Future<void> setPageCollectionModeManual() async {
-    if (_supportPlatform()) return;
-    _channel.invokeMethod<dynamic>('setPageCollectionModeManual');
-  }
+/// 如果需要使用页面统计，则先打开该设置
+Future<void> setPageCollectionModeManual() async {
+  if (_supportPlatform()) return;
+  _channel.invokeMethod<dynamic>('setPageCollectionModeManual');
+}
 
-  /// 进入页面统计
-  static Future<void> onPageStart(String pageName) async {
-    if (_supportPlatform()) return;
-    _channel.invokeMethod<dynamic>('onPageStart', <String, String>{'pageName': pageName});
-  }
+/// 进入页面统计
+Future<void> onPageStartUM(String pageName) async {
+  if (_supportPlatform()) return;
+  _channel.invokeMethod<dynamic>('onPageStart', <String, String>{'pageName': pageName});
+}
 
-  /// 离开页面统计
-  static Future<void> onPageEnd(String pageName) async {
-    if (_supportPlatform()) return;
-    _channel.invokeMethod<dynamic>('onPageEnd', <String, String>{'pageName': pageName});
-  }
+/// 离开页面统计
+Future<void> onPageEndUM(String pageName) async {
+  if (_supportPlatform()) return;
+  _channel.invokeMethod<dynamic>('onPageEnd', <String, String>{'pageName': pageName});
+}
 
-  /// 如果不需要上述页面统计，在完成后可关闭该设置；如果没有用该功能可忽略；
-  static Future<void> setPageCollectionModeAuto() async {
-    if (_supportPlatform()) return;
-    _channel.invokeMethod<dynamic>('setPageCollectionModeAuto');
-  }
+/// 如果不需要上述页面统计，在完成后可关闭该设置；如果没有用该功能可忽略；
+Future<void> setPageCollectionModeAutoUM() async {
+  if (_supportPlatform()) return;
+  _channel.invokeMethod<dynamic>('setPageCollectionModeAuto');
+}
 
-  /// 错误发送
-  static Future<void> reportError(String error) async {
-    if (_supportPlatform()) return;
-    _channel.invokeMethod<dynamic>('reportError', <String, String>{'error': error});
-  }
+/// 错误发送
+Future<void> reportErrorUM(String error) async {
+  if (_supportPlatform()) return;
+  _channel.invokeMethod<dynamic>('reportError', <String, String>{'error': error});
 }
 
 bool _supportPlatform() {
   if (!(Platform.isAndroid || Platform.isIOS)) {
-    print('flutter_umeng  is not support ${Platform.operatingSystem}');
+    print('fl_umeng is not support ${Platform.operatingSystem}');
     return true;
   }
   return false;
