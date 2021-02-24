fa46a6967f74aad30edc3c380959e1bb59b583c6
Wayaer <wayaer@foxmail.com>
2021-02-24 14:57:53 +0800

    android ios

--- android/src/main/kotlin/flutter/umeng/UmengPlugin.kt
+++ android/src/main/kotlin/flutter/umeng/UmengPlugin.kt
@@ -1,60 +1,52 @@
 package flutter.umeng
 
-import android.content.Context
 import com.umeng.analytics.MobclickAgent
 import com.umeng.commonsdk.UMConfigure
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.embedding.engine.plugins.FlutterPlugin.FlutterPluginBinding
-import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
-import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 
 
-class UmengPlugin : FlutterPlugin, MethodCallHandler {
-    private var context: Context? = null
+class UmengPlugin : FlutterPlugin {
+
     private lateinit var channel: MethodChannel
+
     override fun onAttachedToEngine(plugin: FlutterPluginBinding) {
         channel = MethodChannel(plugin.binaryMessenger, "UMeng")
-        channel.setMethodCallHandler(this)
-        context = plugin.applicationContext
-    }
+        val context = plugin.applicationContext
+        channel.setMethodCallHandler { call, result ->
+            when (call.method) {
+                "init" -> {
+                    val androidAppKey = call.argument<String>("androidAppKey")
+                    val channel = call.argument<String>("channel")
+                    UMConfigure.init(context, androidAppKey, channel, UMConfigure.DEVICE_TYPE_PHONE, null);
+                }
+                "onEvent" -> {
+                    val event = call.argument<String>("event")
+                    val map = call.argument<Map<String, *>>("properties")
+                    MobclickAgent.onEventObject(context, event, map)
+                }
+                "setLogEnabled" -> {
+                    val logEnabled = call.argument<Boolean>("logEnabled")
+                    if (logEnabled != null) UMConfigure.setLogEnabled(logEnabled)
+                }
+                "onProfileSignIn" -> MobclickAgent.onProfileSignIn(call.argument("userID"))
+                "onProfileSignOff" -> MobclickAgent.onProfileSignOff()
+                "setPageCollectionModeAuto" -> MobclickAgent.setPageCollectionMode(MobclickAgent.PageMode.LEGACY_AUTO)
+                "setPageCollectionModeManual" -> MobclickAgent.setPageCollectionMode(MobclickAgent.PageMode.LEGACY_MANUAL)
+                "onPageStart" -> MobclickAgent.onPageStart(call.argument("pageName"))
+                "onPageEnd" -> MobclickAgent.onPageEnd(call.argument("pageName"))
+                "reportError" -> MobclickAgent.reportError(context, call.argument<String>("error"))
+                else -> result.notImplemented()
+            }
 
-    override fun onMethodCall(call: MethodCall, result: MethodChannel.Result) {
-        when (call.method) {
-            "init" -> initCommon(call)
-            "onEvent" -> onEvent(call)
-            "setLogEnabled" -> setLogEnabled(call)
-            "onProfileSignIn" -> MobclickAgent.onProfileSignIn(call.argument("userID"))
-            "onProfileSignOff" -> MobclickAgent.onProfileSignOff()
-            "setPageCollectionModeAuto" -> MobclickAgent.setPageCollectionMode(MobclickAgent.PageMode.LEGACY_AUTO)
-            "setPageCollectionModeManual" -> MobclickAgent.setPageCollectionMode(MobclickAgent.PageMode.LEGACY_MANUAL)
-            "onPageStart" -> MobclickAgent.onPageStart(call.argument("pageName"))
-            "onPageEnd" -> MobclickAgent.onPageEnd(call.argument("pageName"))
-            "reportError" -> MobclickAgent.reportError(context, call.argument<String>("error"))
-            else -> result.notImplemented()
         }
+
 
     }
 
     override fun onDetachedFromEngine(binding: FlutterPluginBinding) {
         channel.setMethodCallHandler(null)
-    }
-
-    private fun setLogEnabled(call: MethodCall) {
-        val logEnabled = call.argument<Boolean>("logEnabled")
-        if (logEnabled != null) UMConfigure.setLogEnabled(logEnabled)
-    }
-
-    private fun initCommon(call: MethodCall) {
-        val androidAppKey = call.argument<String>("androidAppKey")
-        val channel = call.argument<String>("channel")
-        UMConfigure.init(context, androidAppKey, channel, UMConfigure.DEVICE_TYPE_PHONE, null);
-    }
-
-    private fun onEvent(call: MethodCall) {
-        val event = call.argument<String>("event")
-        val map = call.argument<Map<String, *>>("properties")
-        MobclickAgent.onEventObject(context, event, map)
     }
 
 }
--- ios/Classes/UmengPlugin.m
+++ ios/Classes/UmengPlugin.m
@@ -4,41 +4,38 @@
 #import <UMCommon/MobClick.h>
 
 @implementation UmengPlugin
+
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
     FlutterMethodChannel* channel = [FlutterMethodChannel
                                      methodChannelWithName:@"UMeng"
-                                     binaryMessenger:[registrar messenger]];
-    UmengPlugin* plugin = [[UmengPlugin alloc] init];
-    [registrar addMethodCallDelegate:plugin channel:channel];
+                                     binaryMessenger:registrar.messenger];
+    [channel setMethodCallHandler:^(FlutterMethodCall *  call, FlutterResult  result) {
+        NSDictionary *args = call.arguments;
+        if ([@"init" isEqualToString:call.method]){
+            [UMConfigure initWithAppkey:args[@"iosAppKey"] channel:args[@"channel"]];
+        }else if ([@"setLogEnabled" isEqualToString:call.method]){
+            [UMCommonLogManager setUpUMCommonLogManager];
+            BOOL logEnabled = [[args objectForKey:@"logEnabled"] boolValue];
+            [UMConfigure setLogEnabled:logEnabled];
+        }else if([@"onEvent" isEqualToString:call.method]){
+            [MobClick event:args[@"event"] attributes:args[@"properties"]];
+        }else if ([@"onProfileSignIn" isEqualToString:call.method]){
+            [MobClick profileSignInWithPUID:args[@"userID"]];
+        }else if ([@"onProfileSignOff" isEqualToString:call.method]){
+            [MobClick profileSignOff];
+        }else if ([@"setPageCollectionModeAuto" isEqualToString:call.method]){
+            [MobClick setAutoPageEnabled:YES];
+        }else if ([@"setPageCollectionModeManual" isEqualToString:call.method]){
+            [MobClick setAutoPageEnabled:NO];
+        }else if ([@"onPageStart" isEqualToString:call.method]){
+            [MobClick beginLogPageView:args[@"pageName"]];
+        }else if ([@"onPageEnd" isEqualToString:call.method]){
+            [MobClick endLogPageView:args[@"pageName"]];
+        }else if ([@"reportError" isEqualToString:call.method]){
+            NSLog(@"reportError API not existed ");
+        }else{
+            result(FlutterMethodNotImplemented);
+        }
+    }];
 }
-- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
-    NSDictionary *args = call.arguments;
-    if ([@"init" isEqualToString:call.method]){
-        [UMConfigure initWithAppkey:args[@"iosAppKey"] channel:args[@"channel"]];
-    }else if ([@"setLogEnabled" isEqualToString:call.method]){
-        [UMCommonLogManager setUpUMCommonLogManager];
-        BOOL logEnabled = [[args objectForKey:@"logEnabled"] boolValue];
-        [UMConfigure setLogEnabled:logEnabled];
-    }else if([@"onEvent" isEqualToString:call.method]){
-        [MobClick event:args[@"event"] attributes:args[@"properties"]];
-    }else if ([@"onProfileSignIn" isEqualToString:call.method]){
-        [MobClick profileSignInWithPUID:args[@"userID"]];
-    }else if ([@"onProfileSignOff" isEqualToString:call.method]){
-        [MobClick profileSignOff];
-    }else if ([@"setPageCollectionModeAuto" isEqualToString:call.method]){
-        [MobClick setAutoPageEnabled:YES];
-    }else if ([@"setPageCollectionModeManual" isEqualToString:call.method]){
-        [MobClick setAutoPageEnabled:NO];
-    }else if ([@"onPageStart" isEqualToString:call.method]){
-        [MobClick beginLogPageView:args[@"pageName"]];
-    }else if ([@"onPageEnd" isEqualToString:call.method]){
-        [MobClick endLogPageView:args[@"pageName"]];
-    }else if ([@"reportError" isEqualToString:call.method]){
-        NSLog(@"reportError API not existed ");
-    }else{
-        result(FlutterMethodNotImplemented);
-    }
-    
-}
-
 @end
